48746fe8a33e197afa389d4c76b93fc29d080305
AndriyAtWork <andriytsar3388@gmail.com>
2026-03-10 16:11:46 +0100

    chore: clean project structure and create base layout

---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <div className="container">
+          <p>Autoservice Ehra</p>
+        </div>
+      </nav>
+    </header>
+  );
+}
---
+++ components/sections/Hero.tsx
@@ -0,0 +1,13 @@
+// Hero ist der erste große sichtbare Bereich auf der Startseite.
+// Hier kommen später Logo, Überschrift, Text und Buttons rein.
+
+export default function Hero() {
+  return (
+    <section>
+      <div className="container">
+        <h1>Autoservice Ehra</h1>
+        <p>KFZ & Motorrad Service</p>
+      </div>
+    </section>
+  );
+}
